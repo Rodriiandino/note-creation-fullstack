7f108e3a4afb8c5da95b299b265a3663cdf6038b
RodriiAndino <andinorodrigo03@gmail.com>
2024-04-25 18:27:09 -0300

    add listeners, userDetails, principal to user for authentication

--- note-creation-backend/src/main/java/com/challenge/note/domain/model/User.java
+++ note-creation-backend/src/main/java/com/challenge/note/domain/model/User.java
@@ -3,15 +3,30 @@ package com.challenge.note.domain.model;
 import com.challenge.note.domain.dto.User.CreateUserDTO;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
 @EqualsAndHashCode(of = "id")
-public class User {
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,9 +34,59 @@ public class User {
     @Column(unique = true) 
     private String username;
     private String password;
+    @Column(unique = true)
+    private String email;
+    private boolean account_locked;
+    private boolean enabled;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private List<Role> roles;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModified_at;
+
 
     public User(CreateUserDTO createUserDTO) {
         this.username = createUserDTO.username();
         this.password = createUserDTO.password();
+    }
+
+    @Override
+    public String getName() {
+        return username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !account_locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
     }
 }

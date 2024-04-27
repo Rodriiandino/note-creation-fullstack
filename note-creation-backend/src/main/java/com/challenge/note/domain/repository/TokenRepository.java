5ecc7af58ed58c20084c5a581128810e72ec7dc8
RodriiAndino <andinorodrigo03@gmail.com>
2024-04-26 23:09:08 -0300

    change to no add token to db

--- note-creation-backend/src/main/java/com/challenge/note/domain/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.note.domain.repository;
-
-import com.challenge.note.domain.model.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-
-    Token findByToken(String token);
-}
---
+++ note-creation-backend/src/main/resources/db/migration/V2__create_table_role.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    role TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);

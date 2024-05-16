3b2f5f9a3e39e93639f808dad46595db13fabf36
RodriiAndino <andinorodrigo03@gmail.com>
2024-05-16 15:08:01 -0300

    Remove unused SVG icons and add LinkBack component

---
+++ note-creation-frontend/src/components/link-back.tsx
@@ -0,0 +1,23 @@
+import '../styles/global.css'
+import { Link } from 'react-router-dom'
+
+export default function LinkBack({ path }: { path: string }) {
+  return (
+    <Link className='account__back' to={path}>
+      <svg
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke-width='1.5'
+        stroke='currentColor'
+        width='20'
+        height='20'
+      >
+        <path
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          d='M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3'
+        />
+      </svg>
+    </Link>
+  )
+}
